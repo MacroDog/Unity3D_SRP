f587afa8ab5c020841eccd54d44dfa0dcfbe96ed
xieyifeng <yfxie@uuzu.com>
2019-09-30 17:45:16 +0800

    Basepipeline

---
+++ Assets/RenderPipeline/01_Base/Scripts/McBasePipelineAsset.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Render pipeline/Base")]
+public class McBasePipelineAsset : RenderPipelineAsset
+{
+    protected override RenderPipeline CreatePipeline()
+    {
+        return new McBasePipeline();
+    }
+}
+
+public class McBasePipeline : RenderPipeline
+{
+    public readonly ShaderTagId m_ShaderTagId = new ShaderTagId("01BasePipeline");
+    public CommandBuffer commandBuffer;
+
+    public McBasePipeline()
+    {
+        commandBuffer = new CommandBuffer();
+    }
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        commandBuffer.Release();
+    }
+
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        BeginFrameRendering(cameras);
+        foreach (var camera in cameras)
+        {
+            BeginCameraRendering(camera);
+            Render(context, camera);
+        }
+    }
+
+    private void Render(ScriptableRenderContext context, Camera camera)
+    {
+        ScriptableCullingParameters cullingParameters;
+        if (!camera.TryGetCullingParameters(out cullingParameters))
+        {
+            return;
+        }
+
+        CullingResults cullingResults = context.Cull(ref cullingParameters);
+
+        context.SetupCameraProperties(camera);
+
+        // commandBuff
+        commandBuffer.Clear();
+        commandBuffer.ClearRenderTarget((CameraClearFlags.Depth & camera.clearFlags) != 0, (CameraClearFlags.Color & camera.clearFlags) != 0, camera.backgroundColor, camera.depth);
+        context.ExecuteCommandBuffer(commandBuffer);
+        
+        //init
+        SortingSettings sortingSettings = new SortingSettings(camera);
+        DrawingSettings drawSettings = new DrawingSettings(m_ShaderTagId, sortingSettings);
+        FilteringSettings filterSettings = new FilteringSettings(RenderQueueRange.all);
+
+        //Draw Skybox
+        context.DrawSkybox(camera);
+
+        // Draw opaque
+        sortingSettings.criteria = SortingCriteria.None;
+        drawSettings.sortingSettings = sortingSettings;
+        filterSettings.renderQueueRange = RenderQueueRange.opaque;
+        context.DrawRenderers(cullingResults, ref drawSettings, ref filterSettings);
+
+        // Draw Transparent
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawSettings.sortingSettings = sortingSettings;
+        filterSettings.renderQueueRange = RenderQueueRange.transparent;
+        context.DrawRenderers(cullingResults, ref drawSettings, ref filterSettings);
+        
+        context.Submit();
+    }
+}
---
+++ Assets/RenderPipeline/01_Base/Shader/UnlitTransparent.shader
@@ -0,0 +1,54 @@
+﻿Shader "Pipeline/Base/UnlitTransparent"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Alpha("Alpha",Range(0,1)) = 0.6
+    }
+    SubShader
+    {
+        Tags { "RenderQueue" = "Transparent" }
+        Cull Off Zwrite Off
+        Blend SrcAlpha OneMinusSrcAlpha
+        Pass
+        {
+            Tags{"LightMode"="01BasePipeline"}
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            float _Alpha;
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = float4(tex2D(_MainTex, i.uv).rgb,_Alpha) ;
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
